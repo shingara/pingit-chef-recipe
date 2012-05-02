bcc7636266249cb636bacbae86fd600b1f4b39d1
Cyril Mougel <cyril.mougel@gmail.com>
2012-05-02 23:35:39 +0200

    add nginx configuration in dev mode

--- recipes/development.rb
+++ recipes/development.rb
@@ -7,6 +7,7 @@ include_recipe "rbenv::system_install"
 
 node[:pingit] ||= {}
 node[:pingit][:ruby_version] = "1.9.3-p194"
+node[:pingit][:web_port] = "3000"
 #node[:pingit][:ruby_version] = "jruby-1.6.7"
 
 rbenv_ruby node[:pingit][:ruby_version] do
@@ -42,7 +43,7 @@ export PATH=/usr/local/rbenv/shims/:/usr/local/rbenv/bin:$PATH
 export RBENV_VERSION=#{node[:pingit][:ruby_version]}
 export RAILS_ENV=development
 export UNICORN_WORKER_PROCESS=1
-export UNICORN_LISTEN=3000
+export UNICORN_LISTEN=#{node[:pingit][:web_port]}
 export UNICORN_PID=/var/run/pingit/unicorn.pid
 export REDIS_URL=redis://localhost:6379
 export UNICORN_STDOUT_PATH=/vagrant/log/unicorn_stdout.log
@@ -51,3 +52,14 @@ export UNICORN_STDERR_PATH=/vagrant/log/unicorn_stderr.log
 end
 
 monitrc 'pingit_dev'
+
+template "/etc/nginx/sites-available/pingit_dev.conf" do
+  source 'pingit_dev.nginx.erb'
+  variables({
+    :listen_port => node[:pingit][:web_port],
+    :host => 'pingit.dev',
+    :public_path => '/vagrant/public'
+  })
+end
+
+nginx_site 'pingit_dev.conf'
